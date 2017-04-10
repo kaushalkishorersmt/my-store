5c1fcf94a2c77daba55938cdee9e57c1493c4c5e
kaushalkishorersmt <kaushalkishorersmt@gmail.com>
2017-04-11 03:14:54 +0530

    Add carts and price details.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'pages#index'
-
   get 'shop' => 'pages#shop'
+
+  resources :carts
+  resources :product_items
+
+
 end
